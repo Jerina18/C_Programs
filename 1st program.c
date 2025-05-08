2c291872493978b403e26318c43c29d46ac1daf3
Jerina18 <sharmijerina@gmail.com>
2025-05-08 10:10:46 +0530

    Add files via upload

---
+++ 1st program.c
@@ -0,0 +1,14 @@
+/* A simple program in C
+   Written by Sharmi Jerina  Prithina dated 05-05-2004 */
+   
+#include<stdio.h>
+ 
+int main(){
+     
+     printf("Sharmi Jerina");
+     
+     return 0;
+ }
+ 
+Output
+   Sharmi Jerina
---
+++ 2nd program.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+int main(){
+    
+    int a,b;
+    
+     printf("Enter the two numbers:"); 
+     
+     scanf("%d %d",&a,&b);
+     
+     printf("%d",a+b);
+     
+     return 0;
+ }
+
+Output
+Enter the two numbers:24
+18
+42
